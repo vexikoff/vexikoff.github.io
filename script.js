960d58e72a59a716845c4f70a93241db3d71e670
Vexikoff <vexikoff@gmail.com>
2026-05-07 16:11:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const body = document.body;
 
+if (localStorage.getItem('theme') === 'darktheme') {
+  body.classList.add('darktheme');
+}
+
 function tht() {
   body.classList.toggle('darktheme');
+  
+  if (body.classList.contains('darktheme')) {
+    localStorage.setItem('theme', 'darktheme');
+  } else {
+    localStorage.removeItem('theme');
+  }
 }
 
 function goto(link) {
