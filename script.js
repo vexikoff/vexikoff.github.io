de53d11d0eea220323cd66d1d68278830eea622c
Vexikoff <vexikoff@gmail.com>
2026-05-09 19:45:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const body = document.body;
 
+if (localStorage.getItem('theme') === 'darktheme') {
+  body.classList.add('darktheme');
+}
+
 function tht() {
   body.classList.toggle('darktheme');
+  
+  if (body.classList.contains('darktheme')) {
+    localStorage.setItem('theme', 'darktheme');
+  } else {
+    localStorage.removeItem('theme');
+  }
 }
 
 function goto(link) {
   window.location.href = link;
 }
+
 
 function detectDevice() {
   const userAgent = navigator.userAgent;
